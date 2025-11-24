a0a2a989476ae5edbabfbbd1a1ec3ad3fdffb3c6
ascanlindon-dev <ascanlindon@gmail.com>
2025-11-24 13:51:45 +0800

    ok

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
-
 class Auth extends Controller {
 
     public function __construct() {
