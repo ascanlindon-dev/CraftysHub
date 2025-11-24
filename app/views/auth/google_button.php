36635c714da449c5d594780e858853486234d30e
ascanlindon-dev <ascanlindon@gmail.com>
2025-11-24 14:25:32 +0800

    okop

--- app/views/auth/google_button.php
+++ app/views/auth/google_button.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <a href="/auth/google-login" class="btn btn-google" style="display:inline-block;padding:10px 20px;background:#4285F4;color:#fff;border-radius:5px;text-decoration:none;font-weight:500;">
   <img src="https://developers.google.com/identity/images/g-logo.png" style="height:20px;vertical-align:middle;margin-right:8px;"> Sign up with Google
 </a>
@@ -6,4 +6,3 @@
 <a href="/auth/google-login" class="btn btn-google" style="display:inline-block;padding:10px 20px;background:#4285F4;color:#fff;border-radius:5px;text-decoration:none;font-weight:500;">
   <img src="https://developers.google.com/identity/images/g-logo.png" style="height:20px;vertical-align:middle;margin-right:8px;"> Sign up with Google
 </a>
->>>>>>> da170f7 (sure to?)
